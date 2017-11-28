f5fb29c6ffa52ecdaa3666e5c7b1dc3b8c46aa58
AlinkaShuu <alinkalol.sh@yandex.ru>
2017-11-28 13:30:59 +0700

    Исправила замечания

--- index.php
+++ index.php
@@ -26,34 +26,32 @@
 			<input type="submit" name="operation" value="Анализ">
 		</form>
 			 <?php
-			if (isset($_GET['operation']) && $value1 != '' && $value2 != '' && $value3 != '') {
-				if (!(INT)($value1) || $value1<=0  || !(INT)($value2) || $value2<=0 || !(INT)($value3) || $value3<=0 ) {
-					echo "Ошибка при вводе данных!";
-				}
-				else{
-					$s1 = number_format($value1 * $value2, 2, ',', ' ');
-					$s2 = number_format($value1 * $value3, 2, ',', ' ');
-					
-						if ($s1 > $s2) {
-						$result = $s1;
-						$comment='под номером 3,4!';
-						} else if ($s2 > $s1) {
-						$result = $s2;
-						$comment='под номером 1,2!';
-						}
-						else {
-						$result = $s1; 
-						$comment='Комната квадратная, все стены равны!';
-						}
-						echo "<p>"."Площадь наибольшей стены = ".htmlspecialchars($result)."  "." м.кв. "."  ".htmlspecialchars($comment)."</p>";
-						
+				if (isset($_GET['operation']) && $value1 != '' && $value2 != '' && $value3 != '') {
+					if (!(INT)($value1) || $value1<=0  || !(INT)($value2) || $value2<=0 || !(INT)($value3) || $value3<=0 ) {
+						echo "Ошибка при вводе данных!";
 					}
+					else{
+						$s1 = number_format($value1 * $value2, 2, ',', ' ');
+						$s2 = number_format($value1 * $value3, 2, ',', ' ');
 						
-			}
-			else{
-				echo "Не все поля заполнены!";
-			}
-						
+							if ($s1 > $s2) {
+								$result = $s1;
+								$comment='под номером 3,4!';
+							} else if ($s2 > $s1) {
+								$result = $s2;
+								$comment='под номером 1,2!';
+							}
+							else {
+								$result = $s1; 
+								$comment='Комната квадратная, все стены равны!';
+							}
+							echo "<p>"."Площадь наибольшей стены = ".htmlspecialchars($result)."  "." м.кв. "."  ".htmlspecialchars($comment)."</p>";
+							
+						}
+				}
+				else if ($value1 != '' || $value2 != '' || $value3 != '') {
+					echo "Не все поля заполнены!";
+				}
 			?>
 		</body>
 </html>
